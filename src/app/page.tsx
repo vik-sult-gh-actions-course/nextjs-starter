d695e00c1e89e54cdbdc176af8e3fe76fb31dd75
viktoriia.sultanovska <viktoriia.sultanovska@gmail.com>
2025-05-20 21:41:28 +0200

    Test code formatting failure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,13 @@ import Link from "next/link";
 import flagsmith from "@/utils/flagsmith";
 
 export const revalidate = 0;
-sdasdad
+
 export default async function Home() {
   const flags = await flagsmith.getEnvironmentFlags();
   return (
     <main className="main">
       <div>
-        <h2>Hello World</h2>
+   <h2>Hello World</h2>
       </div>
       <Link href="/about">About</Link>
       <br />
